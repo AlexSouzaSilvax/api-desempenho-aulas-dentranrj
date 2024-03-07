423e2c0704ce9d1a111a12ee17fe04b020481887
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-03-07 15:26:00 -0300

    first commit

---
+++ api-desempenho-aulas-detranrj/src/main/java/com/desempenho/aulas/detranrj/api/apidesempenhoaulasdetranrj/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.desempenho.aulas.detranrj.api.apidesempenhoaulasdetranrj;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiDesempenhoAulasDetranrjApplication.class);
+	}
+
+}
---
+++ api-desempenho-aulas-detranrj/src/test/java/com/desempenho/aulas/detranrj/api/apidesempenhoaulasdetranrj/ApiDesempenhoAulasDetranrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desempenho.aulas.detranrj.api.apidesempenhoaulasdetranrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesempenhoAulasDetranrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
