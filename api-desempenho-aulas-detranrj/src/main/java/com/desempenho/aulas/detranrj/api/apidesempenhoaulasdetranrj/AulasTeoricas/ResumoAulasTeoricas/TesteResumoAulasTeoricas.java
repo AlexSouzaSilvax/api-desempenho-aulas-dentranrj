9e8b61343aabf25a38b961798484d593719a71b3
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-03-07 18:27:29 -0300

    ajusted

--- api-desempenho-aulas-detranrj/src/main/java/com/desempenho/aulas/detranrj/api/apidesempenhoaulasdetranrj/AulasTeoricas/ResumoAulasTeoricas/TesteResumoAulasTeoricas.java
+++ api-desempenho-aulas-detranrj/src/main/java/com/desempenho/aulas/detranrj/api/apidesempenhoaulasdetranrj/AulasTeoricas/ResumoAulasTeoricas/TesteResumoAulasTeoricas.java
@@ -7,34 +7,32 @@ public class TesteResumoAulasTeoricas {
 
     public static void main(String[] args) throws IOException {
 
-    	ResumoAulasTeoricasService disciplinaController = new ResumoAulasTeoricasService();
- 
-        //alex 
-       // String renach = "332806847";
+        ResumoAulasTeoricasService disciplinaController = new ResumoAulasTeoricasService();
 
-        //mae 
-       // String renach = "490994679";
+        // alex
+        // String renach = "332806847";
 
-        //joao 
-       // String renach = "491165714";
+        // mae
+        // String renach = "490994679";
 
-        String renach =   "491165714";
+        // joao
+        String renach = "491165714";
 
-       List<ResumoAulasTeoricasBean> disciplinaBeans = disciplinaController.convertRetornoResumo(disciplinaController.requestResumoAulas(renach));
+        List<ResumoAulasTeoricasBean> disciplinaBeans = disciplinaController
+                .convertRetornoResumo(disciplinaController.requestResumoAulas(renach));
 
-       for(int i = 0; i < disciplinaBeans.size(); i++) {
-          System.out.println(disciplinaBeans.get(i).getQuantidade() + " - " + disciplinaBeans.get(i).getNome());
-       }
-/*
-Dados retornados dia 02/09/21 02:16
-17 - Legislação De Trânsito
-4 - Primeiros Socorros
-13 - Direção Defensiva
-4 - Meio Ambiente Cidadania
-3 - Noções De Mecânica Veicular
-
-*/
+        for (int i = 0; i < disciplinaBeans.size(); i++) {
+            System.out.println(disciplinaBeans.get(i).getQuantidade() + " - " + disciplinaBeans.get(i).getNome());
+        }
+        /*
+         * Dados retornados dia 02/09/21 02:16
+         * 17 - Legislação De Trânsito
+         * 4 - Primeiros Socorros
+         * 13 - Direção Defensiva
+         * 4 - Meio Ambiente Cidadania
+         * 3 - Noções De Mecânica Veicular
+         * 
+         */
     }
-
 
 }
