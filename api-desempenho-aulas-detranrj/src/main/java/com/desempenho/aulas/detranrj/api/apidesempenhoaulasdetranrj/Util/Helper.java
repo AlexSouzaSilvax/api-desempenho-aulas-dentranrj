4b5f0a7d2f6d56ff42806768b4e7573a8e51026f
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2025-01-22 17:37:46 -0300

    correcao string errada vindo do detran

--- api-desempenho-aulas-detranrj/src/main/java/com/desempenho/aulas/detranrj/api/apidesempenhoaulasdetranrj/Util/Helper.java
+++ api-desempenho-aulas-detranrj/src/main/java/com/desempenho/aulas/detranrj/api/apidesempenhoaulasdetranrj/Util/Helper.java
@@ -1,19 +1,36 @@
 package com.desempenho.aulas.detranrj.api.apidesempenhoaulasdetranrj.Util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.lang3.text.WordUtils;
 
 @SuppressWarnings("deprecation")
 public class Helper {
 	public static boolean validNumber(String valor) {
-		if (!valor.isEmpty() && !valor.equalsIgnoreCase(null) && valor.matches("[0-9]*")) {
-			return true;
-		}
-		return false;
+		return !valor.isEmpty() && !valor.equalsIgnoreCase(null) && valor.matches("[0-9]*");
 	}
 
 	public static String formatText(String valor) {
-		// return valor.toUpperCase().charAt(0) +
-		// valor.substring(1,valor.length()).toLowerCase();
 		return WordUtils.capitalizeFully(valor);
 	}
+
+	public static String correcaoString(String entrada) {
+
+		Map<String, String> strings = new HashMap<>();
+		strings.put("Dire", "Direção Defensiva");
+		strings.put("No", "Noções de Mecânica");
+		strings.put("Legisla", "Legislação de Trânsito");
+
+		for (Map.Entry<String, String> entry : strings.entrySet()) {
+			String pattern = entry.getKey();
+			String saida = entry.getValue();
+
+			if (entrada.contains(pattern)) {
+				return saida;
+			}
+		}
+		return entrada;
+	}
+
 }
